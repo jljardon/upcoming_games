176976037f2bf937565a0f2cdc3aa453664bc60d
jljardon <github email address>
2018-02-07 18:17:56 -0500

    first commit

---
+++ lib/upcoming_games.rb
@@ -0,0 +1,5 @@
+require "upcoming_games/version"
+
+module UpcomingGames
+  # Your code goes here...
+end
---
+++ spec/upcoming_games_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe UpcomingGames do
+  it "has a version number" do
+    expect(UpcomingGames::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
