d80b12738477c5a52047082ad996fe786a8e4f2f
jljardon <github email address>
2018-02-09 01:51:12 -0500

    add format to CLI.
    Completed the interface; added help messages and format. Added loop to exit at user's request. Added "Game not found" message.

--- lib/upcoming_games/cli.rb
+++ lib/upcoming_games/cli.rb
@@ -1,33 +1,45 @@
 class UpcomingGames::CLI
    def call
+      start
+   end
+
+   def start
+      puts '******************************************'.yellow.blink
+      puts '****  Welcome to upcoming_games gem!  ****'.cyan
+      puts "******************************************\n\n".yellow.blink
+
       list_upcoming_games
-      puts "\nPlease input the name of the game you wish to see:\n"
-      game_title = gets.strip
-      selected_game(UpcomingGames::Game.find_by_title(game_title))
+      input = ""
+      until input.casecmp('exit').zero?
+         puts "\nPlease input the name of the game you wish to see:\n"
+         puts '(Or type exit to terminate the program.)'
+         input = gets.strip
+         break if input == 'exit'
+         !UpcomingGames::Game.find_by_title(input).nil? ? selected_game(UpcomingGames::Game.find_by_title(input)) : (puts "\nGame not found!".white.on_red.blink)
+      end
    end
 
    def list_upcoming_games
-      puts "Welcome to upcoming_games gem!\n".cyan
-      puts 'Here is a list of upcoming Video Games:'
+      puts "Here is a list of upcoming Video Games:\n"
       UpcomingGames::Scraper.game_list_scraper
       UpcomingGames::Game.all.each do |game|
-         puts "*************************".light_blue
-         puts "Title: ".red + game.title
-         puts "Platform: ".red + game.platform
-         puts "Release date: ".red + game.release_date
-         puts "Genre: ".red + game.genre
+         puts '******************************************'.light_blue
+         puts 'Title: '.red + game.title
+         puts 'Platform: '.red + game.platform
+         puts 'Release date: '.red + game.release_date
+         puts 'Genre: '.red + game.genre
       end
    end
 
    def selected_game(game)
       puts "\n\nHere are more details of the selected game:".blue
       UpcomingGames::Scraper.game_details_scraper(game)
-      puts "Title: ".red + game.title
-      puts "Release date: ".red + game.release_date
-      puts "Genre: ".red + game.genre
-      puts "Platform: ".red + game.platform
-      puts "Rating: ".red + game.rating
-      puts "Publisher: ".red + game.publisher
-      puts "Summary: ".red + game.summary + "\n"
+      puts 'Title: '.red + game.title
+      puts 'Release date: '.red + game.release_date
+      puts 'Genre: '.red + game.genre
+      puts 'Platform: '.red + game.platform
+      puts 'Rating: '.red + game.rating
+      puts 'Publisher: '.red + game.publisher
+      puts 'Summary: '.red + game.summary + "\n"
    end
 end
